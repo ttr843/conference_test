adcb2c10b7298c13ba14dbacbf81779bbfdee11a
Ruslan Pashin <empty-15@mail.ru>
2021-02-02 16:24:25 +0300

    fix bug

--- src/main/java/ru/waveaccess/test/conference/config/DatabaseConfig.java
+++ src/main/java/ru/waveaccess/test/conference/config/DatabaseConfig.java
@@ -1,9 +1,7 @@
 package ru.waveaccess.test.conference.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
 public class DatabaseConfig {
 }
